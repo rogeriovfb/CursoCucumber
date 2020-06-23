a40e208ab6094b3aac750aa248f6cb4d63939179
Rogerio Ballestrin <ballestrin.rogerio@gmail.com>
2020-06-23 15:15:10 -0300

    Teste Cenário: Deve incrementar contador

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -1,3 +1,5 @@
+import org.junit.Assert;
+
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
@@ -16,5 +18,21 @@ public class AprenderCucumber {
 	@Então("^a especificação deve finalizar com sucesso$")
 	public void aEspecificaçãoDeveFinalizarComSucesso() throws Throwable {
 		
+	}
+	private int contador = 0;
+			
+	@Dado("^que o valor do contador é (\\d+)$")
+	public void queOValorDoContadorÉ(int arg1) throws Throwable {
+	    contador = arg1;
+	}
+
+	@Quando("^eu incrementar em (\\d+)$")
+	public void euIncrementarEm(int arg1) throws Throwable {
+	    contador += arg1;
+	}
+
+	@Então("^o valor do contador será (\\d+)$")
+	public void oValorDoContadorSerá(int arg1) throws Throwable {
+	    Assert.assertEquals(arg1, contador);
 	}
 }
